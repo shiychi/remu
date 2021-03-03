2b9b7bb712625dabbe32d66f8607bef6d09eb7c9
shiychi <shiychi@protonmail.ch>
2021-03-03 22:41:21 +0900

    refactor(emulator): use default trait

--- src/emulator.rs
+++ src/emulator.rs
@@ -1,12 +1,9 @@
+#[derive(Default)]
 pub struct Emulator {
     pub memory: Vec<u8>,
 }
 
 impl Emulator {
-    pub fn new() -> Self {
-        Emulator { memory: Vec::new() }
-    }
-
     pub fn write_binary(&mut self, binary_str: Vec<u8>) {
         self.memory = binary_str;
     }
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,13 @@ use remu::emulator::Emulator;
 use std::fs::File;
 use std::io::Read;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
     let binary_path = std::env::args().nth(1).expect("must be set binary path");
     let mut binary_file = File::open(binary_path).expect("failed to open file");
     let mut binary = Vec::new();
     binary_file
         .read_to_end(&mut binary)
         .expect("failed to read file");
-    let mut emulator = Emulator::new();
+    let mut emulator = Emulator::default();
     emulator.write_binary(binary);
-    Ok(())
 }
